845334bf59ea3efe3170116db31d898b93cc5387
lmandu1995 <leandro.brito@sptech.school>
2025-10-06 17:02:45 -0300

    Revert "Merge pull request #24 from Grupo-6-ADS-B/dev-dados-antropometricos"
    This reverts commit d26f095567c147c7ce9d94c411139dab23e1305e, reversing
    changes made to 7f7caacb52ea2cf87eba1df804e499237018d7ac.

--- backend/src/main/java/fitt_nutri/example/demo/repository/DadosAntropometricosRepository.java
+++
@@ -1,6 +0,0 @@
-package fitt_nutri.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DadosAntropometricosRepository extends JpaRepository<DadosAntropometricosRepository, Integer> {
-}
