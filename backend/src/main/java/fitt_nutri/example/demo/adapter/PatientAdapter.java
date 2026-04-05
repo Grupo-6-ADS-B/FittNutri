71b0eb7aad9d85a296ae057700789e56bb609c2c
lmandu1995 <leandro.brito@sptech.school>
2026-04-05 15:55:20 -0300

    'Apagando na PatientAdapter duplicidade do ultimo get'

--- backend/src/main/java/fitt_nutri/example/demo/adapter/PatientAdapter.java
+++ backend/src/main/java/fitt_nutri/example/demo/adapter/PatientAdapter.java
@@ -61,8 +61,7 @@ public class PatientAdapter {
                 p.getEtnia(),
                 p.getAtividade(),
                 p.getMotivoConsulta(),
-                p.getNutricionista().getId()  // retorna o id do nutricionista associado
-                p.getNutricionista().getId()
+                p.getNutricionista().getId()  // retorna o id do nutricionista associad
         );
     }
 }
