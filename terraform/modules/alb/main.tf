5254f4b8159d74cc7321d7ed63a8e05f5b2cab5a
LucasRodriguesCartaxo <lucas.cartaxo@sptech.school>
2026-04-17 16:07:13 -0300

    feat(alb): listener HTTP :80 com redirect 301 para HTTPS

--- terraform/modules/alb/main.tf
+++ terraform/modules/alb/main.tf
@@ -63,3 +63,23 @@ resource "aws_lb_target_group" "grafana" {
     Name = "${var.project}-${var.environment}-grafana-tg"
   })
 }
+
+resource "aws_lb_listener" "http_redirect" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-${var.environment}-http-redirect"
+  })
+}
